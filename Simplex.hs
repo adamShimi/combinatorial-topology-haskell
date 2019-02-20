6cc418f1533e09a4d5df78e1cb79729a44930640
Adam Shimi <user.name>
2019-02-20 23:37:55 +0100

    Add dimension functions and lenses.

--- Simplex.hs
+++ Simplex.hs
@@ -1,23 +1,28 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Simplex
     (Simplex,
      newSimplex) where
 
+import Control.Lens
 import Data.Set as Set
 
 
 
 -- Basic Simplexes
 
-newtype Simplex a = Simplex (Set a)
+newtype Simplex a = Simplex { _simplex :: (Set a) }
     deriving (Eq)
+makeLenses ''Simplex
 
 instance (Show a) => Show (Simplex a) where
-    show (Simplex set) =
-        (++) "{" $ Set.foldr (\new acc -> case acc of
-                                         "}" -> (show new) ++ acc
-                                         _   -> (show new) ++ ";" ++ acc)
-                             "}"
-                             set
+    show =
+        ((++) "{")
+        . (Set.foldr (\new acc -> case acc of
+                                    "}" -> (show new) ++ acc
+                                    _   -> (show new) ++ ";" ++ acc)
+                    "}")
+        . (view simplex)
 
 newSimplex :: (Ord a) => [a] -> Simplex a
 newSimplex = Simplex . Set.fromList
@@ -26,15 +31,18 @@ standardSimplex :: Int -> Simplex Int
 standardSimplex =
     Simplex . Set.fromList . (flip take) [1..]
 
+dimensionSimplex :: Simplex a -> Int
+dimensionSimplex = size . (view simplex)
+
 
 
 -- Processes
 
-data Proc = Proc Int
+data Proc = Proc {num :: Int}
     deriving (Ord, Eq)
 
 instance Show Proc where
-    show (Proc n) = "p" ++ (show n)
+    show = ((++) "p") . show . num
 
 allProcs :: [Proc]
 allProcs = [Proc n | n <- [1..]]
@@ -46,10 +54,11 @@ procs = (flip take) allProcs
 
 -- Colored Simplexes
 
-newtype ColSimplex c a  = ColSimplex (Simplex (c,a))
+newtype ColSimplex c a  = ColSimplex { _colSimplex :: (Simplex (c,a)) }
+makeLenses ''ColSimplex
 
 instance (Show c, Show a) => Show (ColSimplex c a) where
-    show (ColSimplex s) = show s
+    show = show . (view colSimplex)
 
 newColSimplex :: (Ord c, Ord a) => [(c,a)] -> ColSimplex c a
 newColSimplex = ColSimplex . newSimplex
@@ -58,8 +67,11 @@ standardColSimplex :: Int -> ColSimplex Proc Int
 standardColSimplex n =
     ColSimplex . Simplex . Set.fromList $ zip (procs n) (take n [1..])
 
-coloring :: (Ord a, Ord c) => Simplex a -> (a -> c) -> ColSimplex c a
-coloring (Simplex s) f = ColSimplex . Simplex $ Set.map (\x -> (f x,x)) s
+dimensionColSimplex :: ColSimplex c a -> Int
+dimensionColSimplex = dimensionSimplex . (view colSimplex)
+
+coloring :: (Ord a, Ord c) => (a -> c) -> Simplex a -> ColSimplex c a
+coloring f = ColSimplex . (over simplex (Set.map (\x -> (f x,x))))
 
 collapse :: (Ord a, Ord c) => ColSimplex c a -> Simplex a
-collapse (ColSimplex (Simplex s)) = Simplex $ Set.map (snd) s
+collapse = (over simplex (Set.map (snd))) . (view colSimplex)
