df6066ba8afd323884b83cb1a98a3bdd1457f386
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-02-22 18:05:13 +0100

    Simplify Simplex.hs.

--- src/Simplex.hs
+++ src/Simplex.hs
@@ -1,131 +1,19 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE RankNTypes #-}
-
 module Simplex
-    (SSet(SSet),
-    sset,
-    subset,
-    newSset,
-    unionSset,
-    sizeSset,
-    insertSset,
-    deleteSset,
-    differenceSset,
-    mapSset,
-    lookupMaxSset,
-    filterSset,
+    (Proc(Proc),
+    allProcs,
+    procs,
     Simplex(Simplex),
     simplex,
     newSimplex,
     standardSimplex,
     dimSimplex,
     unionSimplex,
-    subsimplex,
-    Proc(Proc),
-    allProcs,
-    procs,
-    ColSimplex(ColSimplex),
-    colSimplex,
-    newColSimplex,
+    subSimplex,
     standardColSimplex,
-    dimColSimplex,
     coloring,
     collapse) where
 
-import Control.Lens
 import Data.Set as Set
-
-
--- My own sets, that behave nicely.
-
-newtype SSet a = SSet { _sset :: (Set a)}
-    deriving (Eq)
-makeLenses ''SSet
-
-instance (Show a) => Show (SSet a) where
-    show =
-        ((++) "{")
-        . (Set.foldr (\new acc -> case acc of
-                                    "}" -> (show new) ++ acc
-                                    _   -> (show new) ++ ";" ++ acc)
-                    "}")
-        . (view sset)
-
-
--- If comparable, then inclusion, if not, lexicographic.
-instance (Ord a) => Ord (SSet a) where
-    (<=) (SSet s1) (SSet s2) =
-        if Set.isSubsetOf s1 s2
-        then True
-        else if Set.isSubsetOf s2 s1
-             then False
-             else (<=) s1 s2
-
-
-subset :: (Ord a) => SSet a -> SSet a -> Bool
-subset (SSet s1) (SSet s2) = Set.isSubsetOf s1 s2
-
-newSset :: (Ord a) => [a] -> SSet a
-newSset = SSet . Set.fromList
-
-unionSset :: (Ord a) => SSet a -> SSet a -> SSet a
-unionSset (SSet s1) (SSet s2) = SSet (Set.union s1 s2)
-
-sizeSset :: (Ord a) => SSet a -> Int
-sizeSset = size . (view sset)
-
-insertSset :: (Ord a) => a -> SSet a -> SSet a
-insertSset e = over sset (Set.insert e)
-
-deleteSset :: (Ord a) => a -> SSet a -> SSet a
-deleteSset e = over sset (Set.delete e)
-
-differenceSset :: (Ord a) => SSet a -> SSet a -> SSet a
-differenceSset s1 = over sset (Set.difference (s1 ^. sset))
-
-mapSset :: (Ord a,Ord b) => (a -> b) -> SSet a -> SSet b
-mapSset f = over sset (Set.map f)
-
-lookupMaxSset :: (Ord a) => SSet a -> Maybe a
-lookupMaxSset = Set.lookupMax . (view sset)
-
-filterSset :: (Ord a) => (a -> Bool) -> SSet a -> SSet a
-filterSset f = over sset (Set.filter f)
-
---foldrSset :: (Ord a, Ord b) => (a -> b -> b) -> b -> SSet a -> b
---foldrSset f init s =
---    (over sset
---          (Set.foldr (\x acc -> Set.union (view simplex x) acc)
---                     Set.empty
---          )
---    )
-
-
--- Basic Simplexes
-
-newtype Simplex a = Simplex { _simplex :: (SSet a) }
-    deriving (Eq,Ord)
-makeLenses ''Simplex
-
-instance (Show a) => Show (Simplex a) where
-    show = show . (view simplex)
-
-newSimplex :: (Ord a) => [a] -> Simplex a
-newSimplex = Simplex . newSset
-
-standardSimplex :: Int -> Simplex Int
-standardSimplex =
-    Simplex . newSset . (flip Prelude.take) [1..]
-
-dimSimplex :: (Ord a) => Simplex a -> Int
-dimSimplex = ((+) (-1)) . sizeSset . (view simplex)
-
-unionSimplex :: (Ord a) => Simplex a -> Simplex a -> Simplex a
-unionSimplex s1 = over simplex (unionSset (s1 ^. simplex))
-
-subsimplex :: (Ord a) => Simplex a -> Simplex a -> Bool
-subsimplex s1 = (subset (s1 ^. simplex)) . (view simplex)
-
 
 -- Processes
 
@@ -142,27 +30,39 @@ procs :: Int -> [Proc]
 procs = (flip Prelude.take) allProcs
 
 
+-- Basic Simplexes
+
+newtype Simplex a = Simplex { simplex :: Set a }
+    deriving (Eq,Ord)
+
+instance (Show a) => Show (Simplex a) where
+    show = show . simplex
+
+newSimplex :: (Ord a) => [a] -> Simplex a
+newSimplex = Simplex . Set.fromList
+
+standardSimplex :: Int -> Simplex Int
+standardSimplex = Simplex . Set.fromList . (flip Prelude.take) [1..]
+
+dimSimplex :: (Ord a) => Simplex a -> Int
+dimSimplex = (+ (-1)) . size . simplex
+
+unionSimplex :: (Ord a) => Simplex a -> Simplex a -> Simplex a
+unionSimplex s1 s2 = Simplex $ Set.union (simplex s1) (simplex s2)
+
+subSimplex :: (Ord a) => Simplex a -> Simplex a -> Bool
+subSimplex s1 s2 = Set.isSubsetOf (simplex s1) (simplex s2)
+
 
 -- Colored Simplexes
 
-newtype ColSimplex c a  = ColSimplex { _colSimplex :: (Simplex (c,a)) }
-makeLenses ''ColSimplex
 
-instance (Show c, Show a) => Show (ColSimplex c a) where
-    show = show . (view colSimplex)
+standardColSimplex :: Int -> Simplex (Proc,Int)
+standardColSimplex n = (Simplex . Set.fromList) $ zip (procs n) [1..n]
 
-newColSimplex :: (Ord c, Ord a) => [(c,a)] -> ColSimplex c a
-newColSimplex = ColSimplex . newSimplex
+coloring :: (Ord a, Ord c) => (a -> c) -> Simplex a -> Simplex (c,a)
+coloring f = Simplex . (Set.map (\x -> (f x,x))) . simplex
 
-standardColSimplex :: Int -> ColSimplex Proc Int
-standardColSimplex n =
-    ColSimplex . newSimplex $ zip (procs n) (Prelude.take n [1..])
+collapse :: (Ord a, Ord c) => Simplex (c,a) -> Simplex a
+collapse = Simplex . (Set.map snd) . simplex
 
-dimColSimplex :: (Ord c, Ord a) => ColSimplex c a -> Int
-dimColSimplex = dimSimplex . (view colSimplex)
-
-coloring :: (Ord a, Ord c) => (a -> c) -> Simplex a -> ColSimplex c a
-coloring f = ColSimplex . (over simplex (mapSset (\x -> (f x,x))))
-
-collapse :: (Ord a, Ord c) => ColSimplex c a -> Simplex a
-collapse = (over simplex (mapSset (snd))) . (view colSimplex)
